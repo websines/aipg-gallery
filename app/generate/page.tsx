bfbd882cb1522b2f9e4b030c667b924083cb9d68
websines <to.subh03@gmail.com>
2024-03-20 16:30:46 +0530

    update-1

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center my-8">hi</div>
+    </>
+  );
 };
 
 export default page;
---
+++ components/image-gen-components/OptionsPanel.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OptionsPanel = () => {
+  return <div>OptionsPanel</div>;
+};
+
+export default OptionsPanel;
