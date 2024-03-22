c073dafa1967799c2d12e3d323c31d93a64a087b
websines <to.subh03@gmail.com>
2024-03-22 20:14:32 +0530

    user-nav-update

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -5,15 +5,18 @@ import ImageGenForm from "@/components/image-gen-components/ImageGenForm";
 import { AlertDestructive } from "@/components/misc-components/AlertDestructive";
 
 const page = () => {
+  const user = true;
   return (
     <div className="w-full flex flex-col gap-4 items-center justify-center p-4">
-      <div className="md:w-[50%]">
-        <AlertDestructive
-          text="You need to log in to use this!"
-          title="Not logged in!"
-          className="bg-red-500 text-white"
-        />
-      </div>
+      {!user && (
+        <div className="md:w-[50%]">
+          <AlertDestructive
+            text="You need to log in to use this!"
+            title="Not logged in!"
+            className="bg-red-500 text-white"
+          />
+        </div>
+      )}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full ">
         <div className="flex flex-col justify-center items-center my-8 w-full order-2 md:order-1">
           <ImageGenForm />
--- components/homepage-components/UserNav.tsx
+++ components/homepage-components/UserNav.tsx
@@ -10,45 +10,60 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogOut, User } from "lucide-react";
+import { LogIn, LogOut, User, User2Icon } from "lucide-react";
 
 export function UserNav() {
+  const user = false;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
             <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarFallback>
+              <User2Icon className="w-4 h-4" />
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
+      {user ? (
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-xs leading-none text-muted-foreground">
+                m@example.com
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem className="cursor-pointer">
+              Profile
+              <DropdownMenuShortcut>
+                <User className="w-4 h-4" />
+              </DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
           <DropdownMenuItem className="cursor-pointer">
-            Profile
+            Log out
             <DropdownMenuShortcut>
-              <User className="w-4 h-4" />
+              <LogOut className="w-4 h-4" />
             </DropdownMenuShortcut>
           </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer">
-          Log out
-          <DropdownMenuShortcut>
-            <LogOut className="w-4 h-4" />
-          </DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
+        </DropdownMenuContent>
+      ) : (
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuItem className="cursor-pointer">
+            Log In
+            <DropdownMenuShortcut>
+              <LogIn className="w-4 h-4" />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      )}
     </DropdownMenu>
   );
 }
