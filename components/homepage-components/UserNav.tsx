9b736efbe29fb2e0e9c08e87d2a558379c5196e6
websines <to.subh03@gmail.com>
2024-03-24 12:19:02 +0530

    auth-form-done

--- components/homepage-components/UserNav.tsx
+++ components/homepage-components/UserNav.tsx
@@ -12,58 +12,75 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { LogIn, LogOut, User, User2Icon } from "lucide-react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { AuthForm } from "../auth-components/AuthForm";
+
 export function UserNav() {
   const user = false;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
-          <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
-            <AvatarFallback>
-              <User2Icon className="w-4 h-4" />
-            </AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      {user ? (
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
-              <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+              <AvatarFallback>
+                <User2Icon className="w-4 h-4" />
+              </AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        {user ? (
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DropdownMenuLabel className="font-normal">
+              <div className="flex flex-col space-y-1">
+                <p className="text-sm font-medium leading-none">shadcn</p>
+                <p className="text-xs leading-none text-muted-foreground">
+                  m@example.com
+                </p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer">
+                Profile
+                <DropdownMenuShortcut>
+                  <User className="w-4 h-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
             <DropdownMenuItem className="cursor-pointer">
-              Profile
+              Log out
               <DropdownMenuShortcut>
-                <User className="w-4 h-4" />
+                <LogOut className="w-4 h-4" />
               </DropdownMenuShortcut>
             </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="cursor-pointer">
-            Log out
-            <DropdownMenuShortcut>
-              <LogOut className="w-4 h-4" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      ) : (
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuItem className="cursor-pointer">
-            Log In
-            <DropdownMenuShortcut>
-              <LogIn className="w-4 h-4" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      )}
-    </DropdownMenu>
+          </DropdownMenuContent>
+        ) : (
+          <DropdownMenuContent className="w-56" align="end" forceMount>
+            <DialogTrigger className="w-full">
+              <DropdownMenuItem className="cursor-pointer">
+                Log In
+                <DropdownMenuShortcut>
+                  <LogIn className="w-4 h-4" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DialogTrigger>
+          </DropdownMenuContent>
+        )}
+      </DropdownMenu>
+      <DialogContent>
+        <AuthForm />
+      </DialogContent>
+    </Dialog>
   );
 }
