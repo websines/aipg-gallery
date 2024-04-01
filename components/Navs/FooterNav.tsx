ee8fb007bf5bc8c77cf01652938e874078e12484
websines <to.subh03@gmail.com>
2024-04-01 14:19:48 +0530

    update

--- components/Navs/FooterNav.tsx
+++ components/Navs/FooterNav.tsx
@@ -8,7 +8,7 @@ const FooterNav = () => {
   const pathname = usePathname();
 
   return (
-    <div className="md:hidden flex fixed bottom-0 py-4 px-2 z-10  w-full bg-zinc-950 backdrop-blur-lg">
+    <div className="md:hidden flex fixed bottom-0 p-2 z-10  w-full bg-zinc-950/30 backdrop-blur-lg">
       <div className="flex flex-row justify-around items-center bg-transparent w-full">
         <Link
           href="/"
