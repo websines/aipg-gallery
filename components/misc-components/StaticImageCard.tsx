7808255586d1a583efaf16650101de51fbf02c34
websines <to.subh03@gmail.com>
2024-06-13 17:40:11 +0530

    update

--- components/misc-components/StaticImageCard.tsx
+++ components/misc-components/StaticImageCard.tsx
@@ -5,7 +5,7 @@ import StaticImagesCarousel from "./StaticImagesCarousel";
 
 const StaticImageCard = ({ images }: { images: any[] }) => {
   if (images.length === 0) return null;
-  const [thumbnail, ...carouselImages] = images;
+  //   const [thumbnail, ...carouselImages] = images;
 
   return (
     <div>
@@ -13,17 +13,17 @@ const StaticImageCard = ({ images }: { images: any[] }) => {
         <DialogTrigger asChild>
           <div className="cursor-pointer relative rounded-sm overflow-hidden group">
             <motion.img
-              src={thumbnail.image_url}
+              src={images[0].image_url}
               className="w-auto h-auto object-cover rounded-sm"
-              alt={thumbnail.seed}
+              alt={images[0].seed}
               whileHover={{ scale: 1.02 }}
               transition={{ type: "spring", stiffness: 200 }}
-            />
+            />{" "}
           </div>
         </DialogTrigger>
         <DialogContent className="md:min-w-[70%] overflow-y-scroll bg-transparent max-h-[80vh] md:max-h-[95vh] no-scrollbar backdrop-blur-md">
           <div className="p-4 flex flex-col-reverse md:flex-row bg-transparent items-center justify-center gap-6 relative backdrop-blur-lg">
-            <StaticImagesCarousel images={carouselImages} />{" "}
+            <StaticImagesCarousel images={images} />
             {/* Pass the rest of the images */}
           </div>
         </DialogContent>
